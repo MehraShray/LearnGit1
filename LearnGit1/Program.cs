cff81e7b8ac17f5a332a5c6dd6a92d7305915e6e
Shray Mehra <104669594+MehraShray@users.noreply.github.com>
2022-05-08 17:56:10 +0530

    Shray/branch2 (#3)
    * New message 2
    
    * Added Employee Class

--- LearnGit1/Program.cs
+++ LearnGit1/Program.cs
@@ -1,5 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using LearnGit1;
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!/ Shray");
 Console.WriteLine("Hello, World!/ Nishant");
 Console.WriteLine("Hello, World!/ Rupali");
+
+Employee e = new Employee();
+e.Id = 1;
+e.Name = "Nishant".ToLower();
+e.Address = "Kangra";
+
+
+if (e.Name == "shray")
+{
+    e.Age = 25;
+
+}
+else if (e.Name == "Nishant".ToLower())
+{
+    e.Age = 27;
+}
+Console.WriteLine("Id:" + e.Id + e.Name + e.Address + e.Age);
